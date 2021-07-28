42b652daa82c8a222aff7885ac585ae81be4e59e
Aabhas Asawa <aabhasasawa52@gmail.com>
2021-07-28 23:39:05 +0530

    Update main application

--- src/main/java/com/project/stockmarket/StockMarketApplication.java
+++ src/main/java/com/project/stockmarket/StockMarketApplication.java
@@ -6,11 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.project.stockmarket.entities.Sector;
-import com.project.stockmarket.entities.StockExchangeEntity;
-import com.project.stockmarket.entities.User;
 import com.project.stockmarket.repositories.CompanyRepository;
 import com.project.stockmarket.repositories.SectorRepository;
 import com.project.stockmarket.repositories.StockExchangeRepository;
@@ -32,8 +28,8 @@ public class StockMarketApplication implements CommandLineRunner {
 	@Autowired
 	StockExchangeRepository exchangeRepository;
 	
-	@Autowired
-	private PasswordEncoder bcryptEncoder;
+//	@Autowired
+//	private PasswordEncoder bcryptEncoder;
 
 //	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -44,20 +40,20 @@ public class StockMarketApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		Sector sector1 = new Sector("Internet Service", "Provide online services to customers");
-		Sector sector2 = new Sector("Finance", "Provide financial services to commercial and retail customers");
-		Sector sector3 = new Sector("Healthcare", "Provide medical services");
-		Sector sector4 = new Sector("Pharmaceutical", "Manufacture and market medicines");
-		Sector sector5 = new Sector("Hotels", "Provide lodging");
-		userRepository.save(new User("admin", bcryptEncoder.encode("admin"), "", "", true, "admin"));
-		userRepository.save(new User("user", bcryptEncoder.encode("user"), "", "", true, "user"));
-		exchangeRepository.save(new StockExchangeEntity("BSE", "Bombay Stock Exchange", "Asia's oldest stock exchange", "Dalal Street, Mumbai", ""));
-		exchangeRepository.save(new StockExchangeEntity("NSE", "National Stock Exchange", "Leading stock exchange of India", "Bandra, Mumbai", ""));
-		sectorRepository.save(sector1);
-		sectorRepository.save(sector2);
-		sectorRepository.save(sector3);
-		sectorRepository.save(sector4);
-		sectorRepository.save(sector5);
+//		Sector sector1 = new Sector("Internet Service", "Provide online services to customers");
+//		Sector sector2 = new Sector("Finance", "Provide financial services to commercial and retail customers");
+//		Sector sector3 = new Sector("Healthcare", "Provide medical services");
+//		Sector sector4 = new Sector("Pharmaceutical", "Manufacture and market medicines");
+//		Sector sector5 = new Sector("Hotels", "Provide lodging");
+//		userRepository.save(new User("admin", bcryptEncoder.encode("admin"), "", "", true, "admin"));
+//		userRepository.save(new User("user", bcryptEncoder.encode("user"), "", "", true, "user"));
+//		exchangeRepository.save(new StockExchangeEntity("BSE", "Bombay Stock Exchange", "Asia's oldest stock exchange", "Dalal Street, Mumbai", ""));
+//		exchangeRepository.save(new StockExchangeEntity("NSE", "National Stock Exchange", "Leading stock exchange of India", "Bandra, Mumbai", ""));
+//		sectorRepository.save(sector1);
+//		sectorRepository.save(sector2);
+//		sectorRepository.save(sector3);
+//		sectorRepository.save(sector4);
+//		sectorRepository.save(sector5);
 	}
 
 }
